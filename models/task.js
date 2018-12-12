da0cefb07b9de95ec2da028d0b6f0932b5f79441
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2018-12-11 18:09:42 -0600

    I think this has the basic fields and associations we'll need. We may need to add some down the line for settling debts - something like a discharged boolean where a task has a price and a different originator and completer. For later, though.

---
+++ models/task.js
@@ -0,0 +1,28 @@
+module.exports = function (sequelize, dataTypes) {
+
+    const Task = sequelize.define("Task", {
+        text: {
+            type: dataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        price: {
+            type: dataTypes.dataType.DECIMAL(10, 2),
+            allowNull: true,
+            validate: {
+                isFloat: true
+            }
+        }
+    });
+    
+    Task.associate = function(models) {
+        Task.belongsTo(models.User, {as: "originator", foreignKey: "originatorId"});
+        Task.belongsTo(models.User, {as: "completer", foreignKey: "completerId"});
+        Task.belongsTo(models.List, {as: "list", foreignKey: "listId"});
+    }
+    
+    return Task;
+}
+
---
+++ models/user.js
@@ -0,0 +1,34 @@
+module.exports = function (sequelize, dataTypes) {
+
+    const User = sequelize.define("User", {
+        userName: {
+            type: dataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        nickName: {
+            type: dataTypes.STRING,
+        },
+        password: {
+            type: dataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    User.addHook("beforeCreate", function(user, options) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(12), null);
+    })
+
+    User.associate = function (models) {
+        User.hasMany(models.Task, {foreignKey: "originatorId", as: "originator"});
+        User.hasMany(models.Task, {foreignKey: "completerId", as: "completer"});
+        User.hasMany(models.List, {foreignKey: "creatorId", as: "creator"});
+        User.belongsToMany(models.List, {through: "listViewers", as: "viewer", foreignKey: "viewerId"});
+        User.belongsToMany(models.List, {through: "listUsers", as: "user", foreignKey: "userId"});
+}
+
+    return User;
+}
+

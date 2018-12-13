f034abac9247abc49d5a7e21282d12a8245e8f43
Brian Duong <brianduongh@gmail.com>
2018-12-13 11:13:39 -0600

    fixing semantic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     //     examples: dbExamples
     //   });
     // });
-    res.render("login");
+    res.render("index");
   });
 
   // Load example page and pass in an example by id

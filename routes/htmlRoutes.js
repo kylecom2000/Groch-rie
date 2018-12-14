a924d32a5d1fedfe03584691eca50332e5c2c2a3
Brian Duong <brianduongh@gmail.com>
2018-12-14 13:44:04 -0600

    declaring api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,23 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
+    res.render("landing");
+  });
+
+  app.get("/login", function(req, res) {
+    res.render("login");
+  });
+
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
+  app.get("/dashboard/user", function(req, res) {
     db.List.findAll({
       include: ["Task"]
     }).then(function(data) {
       console.log(data);
-      res.render("manage", {
+      res.render("dashboard", {
         lists: data
       });
     });

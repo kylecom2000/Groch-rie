70d7230d3ca4fbf8ee7ef961f6f13ed9a9f42ad6
GKJCJG <43424668+GKJCJG@users.noreply.github.com>
2018-12-14 10:40:31 -0600

    socket connection and jury-rigged auth are basically working.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -17,28 +17,6 @@ module.exports = function(app) {
     });
   });
 
-  // This route feeds the user all the data they need to render their dashboard page.
-  // TODO: add handlebars-ready flags to each list item that specify whether that item was originated by the current user - that will affect the user's rights to delete or not.
-  app.get("/user/dash", function (req, res) {
-    const fullData = {};
-
-    db.User.getviewables({ where: { viewerId: req.user.id } })
-      .then(function (data) {
-
-          console.log(data);
-          fullData.viewables = data;
-
-        db.User.getusables({ where: { viewerId: req.user.id } })
-          .then(function (data) {
-
-            console.log(data);
-            fullData.usables = data;
-
-            res.render("", fullData);
-          });
-      });
-  });
-
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
